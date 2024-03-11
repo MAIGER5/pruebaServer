b04e482f229e2c3ddd0760b31174e34fd9faa44a
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2024-03-10 21:35:57 -0400

    otra prueba

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.end('HOLA KIKI estoy haciendote otra prueba')
+    res.end('MEMEBER SHIPOa')
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require('./app.js');
 
+const PORT = 3000;
 
 
-
-server.listen(3000)
+server.listen(PORT, ()=>{
+    console.log("estoy en servidor adecuado")
+});
